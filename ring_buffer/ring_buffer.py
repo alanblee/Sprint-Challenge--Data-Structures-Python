b5c2b5d4a86cdf22681e1570c1b911ff9cb2b9d4
alanblee <alanbenlee12@gmail.com>
2020-06-12 08:42:46 -0700

    tests ok ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        self.current_index = 0
 
     def append(self, item):
-        pass
+        # if the length of the data list is less than capacity, append normally
+        if len(self.data) < self.capacity:
+            self.data.append(item)
+
+        # set the current_index to the item
+        self.data[self.current_index] = item
+        # increment the index for the next item
+        self.current_index += 1
+        # if the index becomes greater than the of capicity -1 (), reset the index to 0
+        if self.current_index > self.capacity - 1:
+            self.current_index = 0
 
     def get(self):
-        pass
+        return self.data
